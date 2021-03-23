7ee471bac5e91c83a115d1824c208f77793d7832
RICKBOT202 <81242313+RICKBOT202@users.noreply.github.com>
2021-03-23 20:43:43 +0700

    lib

---
+++ donasi.js
@@ -0,0 +1,15 @@
+const donasi = () => { 
+	return `       
+⌬ *DONASI*
+----------------------------------------
+⇨ OVO : 0877-1474-5440
+⇨ DANA : 0877-1474-5440
+⇨ PULSA : 0877-1474-5440
+----------------------------------------
+\`\`\`THANKS 4 PARTISIPASI\`\`\`
+
+H a v e  A  N i c e  D a y :>
+`
+}
+
+exports.donasi = donasi
---
+++ listhargadmff.js
@@ -0,0 +1,29 @@
+const listhargadmff = () => { 
+	return `       
+⌬ *LIST HARGA DIAMOND FF*
+----------------------------------------
+⇨ Rp.10.000 = 70 DM
+⇨ Rp.15.000 = 100 DM
+⇨ Rp.20.000 = 140 DM
+⇨ Rp.30.000 = 210 DM
+⇨ Rp.50.000 = 335 DM
+⇨ Rp.68.000 = 500 DM
+⇨ Rp.100.000 = 720 DM
+⇨ Rp.132.000 = 1000 DM
+⇨ Rp.189.000 = 1440 DM
+⇨ Rp.255.000 = 2000 DM
+----------------------------------------
+MEMBERSHIP MINGGUAN
+⇨ Rp.35.000
+----------------------------------------
+MEMBERSHIP BULANAN 
+⇨ Rp.120.000
+----------------------------------------
+
+\`\`\`THANKS 4 PARTISIPASI\`\`\`
+
+H a v e  A  N i c e  D a y :>
+`
+}
+
+exports.listhargadmff = listhargadmff
